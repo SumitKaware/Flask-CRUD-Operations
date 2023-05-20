50d0bf9d60edbb42cb2840853a34cba38715d4ae
SumitKaware <sumit.kaware3@gmail.com>
2023-05-20 23:44:27 +0530

    commit012

--- CRUD_Operations.py
+++ CRUD_Operations.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from contextlib import _RedirectStream
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,9 +17,47 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route("/")
-def message():
-    return render_template('index.html')
+@app.route("/", methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error while adding the task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error while deleting'
+    
+@app.route('/update/<int:id>', methods=['POST','GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error while updating'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Flask-CRUD-Operations
+# First-Interaction
+Start Env - ./env/Scripts/activate.bat
+flask --app CRUD_Operations run
+curl http://localhost:5000
+curl http://localhost:5000/foo
+
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+to set your account's default identity.
+Omit --global to set the identity only in this repository.
+
+fatal: no email was given and auto-detection is disabled
+2023-04-24 00:45:27.912 [info] > git config --get-all user.name [104ms]
+2023-04-24 00:45:28.002 [info] > git config --get commit.template [81ms]
+2023-04-24 00:45:28.014 [info] > git for-each-ref --format=%(refname)%00%(upstream:short)%00%(objectname)%00%(upstream:track)%00%(upstream:remotename)%00%(upstream:remoteref) refs/heads/main refs/remotes/main [86ms]
+2023-04-24 00:45:28.083 [info] > git status -z -uall [59ms]
